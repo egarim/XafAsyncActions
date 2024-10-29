3ce62f44b8abbdd3bde2923c75fb342fc719ad92
Jose Ojeda <joche.ojeda@bitframeworks.com>
2024-10-29 21:55:32 +0300

    Add project files.

---
+++ XafAsyncActions.Blazor.Server/Controllers/MyViewControllerBlazor.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Blazor.Services;
+using DevExpress.ExpressApp.Utils;
+using Microsoft.JSInterop;
+using XafAsyncActions.Module.Controllers;
+
+namespace XafAsyncActions.Blazor.Server.Controllers
+{
+    //HACK https://supportcenter.devexpress.com/ticket/details/t1080389/notify-progress-of-action-execution-changing-the-loading-progress-icon
+    public class MyViewControllerBlazor:MyViewController
+    {
+
+        IJSRuntime jsRuntime;
+        IServiceProvider serviceProvider;
+        ILoadingIndicatorProvider loading;
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            IServiceProvider serviceProvider = ((BlazorApplication)Application).ServiceProvider;
+            jsRuntime = (IJSRuntime)serviceProvider.GetService(typeof(IJSRuntime));
+            loading = serviceProvider.GetService(typeof(ILoadingIndicatorProvider)) as ILoadingIndicatorProvider;
+
+          
+        }
+        protected override void OnDeactivated()
+        {
+            base.OnDeactivated();
+
+        }
+        protected async override void AsyncAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+
+            //HACK show loading indicator
+            loading.Hold("Loading");
+            //Execute the base action on the agnostic module
+            base.AsyncAction_Execute(sender, e);
+        
+
+        }
+        protected async override void ProcessingDone(Dictionary<int, object> results)
+        {
+            //HACK this is in the U.I thread, so we can interact with the U.I and the view and object space of this controller
+            base.ProcessingDone(results);
+            //HACK hide loading indicator
+            loading.Release("Loading");
+           
+        }
+    }
+}
---
+++ XafAsyncActions.Module/Controllers/MyViewController.cs
@@ -0,0 +1,87 @@
+﻿using DevExpress.DataAccess.Sql;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XafSmartEditors.Razor.NqlDotNet;
+
+namespace XafAsyncActions.Module.Controllers
+{
+    public class MyViewController : ViewController
+    {
+        SimpleAction AsyncAction;
+        public MyViewController() : base()
+        {
+            // Target required Views (use the TargetXXX properties) and create their Actions.
+            AsyncAction = new SimpleAction(this, "Async Action", "View");
+            AsyncAction.Execute += AsyncAction_Execute;
+
+        }
+
+        public async Task<string> Task1()
+        {
+            //HACK this task will run on the background thread
+            await Task.Delay(1000);
+            return "Result from Task 1:" + Guid.NewGuid().ToString();
+        }
+
+        public async Task<string> Task2()
+        {
+            //HACK this task will run on the background thread
+            await Task.Delay(2000);
+            return "Result from Task 2:" + Guid.NewGuid().ToString();
+        }
+        protected virtual void ProcessingDone(Dictionary<int, object> results)
+        {
+            //HACK this is in the U.I thread, so we can interact with the U.I and the view and object space of this controller
+        }
+        protected virtual void AsyncAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+
+            //hack we queue a list of tasks to run in the background
+            var tasks = new List<Func<Task<Object>>>
+            {
+                async () => { return await Task1(); },
+                async () => { return await Task2(); },
+                async () => { return await Task1(); },
+                async () => { return await Task2(); },
+                async () => { return await Task2(); },
+                async () => { return await Task2(); },
+            };
+
+            Action<int, string, Object> onProgressChanged = (progress, status, result) =>
+            {
+                //HACK this is in the U.I thread, so we can interact with the U.I and the view and object space of this controller
+                Debug.WriteLine($"{status} - Result: {result}");
+            };
+
+            var worker = new AsyncBackgroundWorker<Object>(
+                tasks,//List of task to run
+                onProgressChanged,//Progress report action
+                result => ProcessingDone(result) //Action to run when all tasks are done
+            );
+
+            worker.Start();
+
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+    }
+}
